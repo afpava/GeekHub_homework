d222395c058429537e19c991add5e97dd22681cf
pVa <pva@netbook>
2018-10-20 01:36:59 +0300

    strings

--- homework1.rb
+++ homework1.rb
@@ -10,6 +10,7 @@ def file_parser(file)
       name = full_name.split
       @last_name << name.pop
       @first_name << name.join(' ')
+      name.class
     end
   else
     puts "File #{file} doesn't exist"
@@ -18,10 +19,10 @@ end
 
 file_parser("names.txt")
 #Array with names
-print "Array with names:#{@names}"
+print "Array with names: \n #{@names}"
 #Getting rid of titles Mr., Mrs., Dr., Jr.
 names = @names.each {|name|  name.gsub!(/Mr[.]|Mrs[.]|Dr[.]|Jr[.]/, "").strip! }
-print "\n\n Names without titles: #{names} \n"
+print "\n\n Names without titles:\n #{names} \n"
 #Creating a Hash: key - is first_name; value - is last_name
 first_name = @first_name.map { |item| item.split(' ').last}
 last_name = @last_name.map { |item| item.split(' ').first}
@@ -32,7 +33,15 @@ print "\n Hash with a key as a first_name and value as last_name :\n #{a}"
 let_first_name = first_name.map { |item| item[0] }
 let_last_name = last_name.map { |item| item[1] }
 
-puts let_first_name.class
+b = [let_first_name, let_last_name].transpose.to_h
 
-b = [let_first_name, let_last_name].transpose.to_a.to_h
-#print "\n Hash with a key as first letter of the first_name and value as the second letter of the last_name :\n #{b}"
+print "\n\n  Hash with a key as first letter of the first_name and value as the second letter of the last_name :\n #{b} \n \n"
+
+names_short = [first_name.map { |item| item[0] }, last_name.map { |item| item[0] }].transpose.to_a.flatten
+
+#Swiching places
+switched = names.map {|item| item.split(' ').reverse.join(' ') }
+
+print "We've switched first_name and last_name: \n #{switched}"
+
+##Integers
