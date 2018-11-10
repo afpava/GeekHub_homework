5c6d6542cea7d54ab79b49f27a17574238051a76
pVa <pva@netbook>
2018-11-10 22:56:13 +0200

    Just a start

---
+++ homeWork4/shop/app/controllers/store_controller.rb
@@ -0,0 +1,51 @@
+class StoreController < ApplicationController
+  def index
+    @stores = Store.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+    redirect_to @store
+  end
+
+  def show
+    @store = Category.new(store: @store)
+  end
+
+  def destroy
+    @store.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_store
+      @store = Store.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def store_params
+      params.require(:store).permit(:name, :image)
+    end
+end
