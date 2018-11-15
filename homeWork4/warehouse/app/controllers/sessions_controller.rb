0d52039dc50a483c465c5bba690605a1be1a6070
pVa <pva@netbook>
2018-11-15 13:46:46 +0200

    Homework 5 - authentification makup

--- homeWork4/warehouse/app/controllers/sessions_controller.rb
+++ homeWork4/warehouse/app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
             user = User.find_by(email: params[:email])
            if user && user.authenticate(params[:password])
              session[:user_id] = user.id
-             redirect_to root_url, notice: 'Logged in!'
+             redirect_to params[:referer], notice: 'Logged in!'
            else
              flash.now.alert = 'Email or password is invalid'
              render :new
            end
     end
-    
+
     def destroy
       session[:user_id] = nil
-      redirect_to root_url, notice: "Logged out!"
+      redirect_back fallback_location: request.referrer, notice: "Logged out!"
     end
 
     private
--- homeWork4/warehouse/app/controllers/users_controller.rb
+++ homeWork4/warehouse/app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ wrap_parameters :user, include: [:name, :password, :password_confirmation]
           @user = User.new(user_params)
             if @user.save
           session[:user_id] = @user.id
-          redirect_to root_url, notice: "Thank you for signing up!"
+          #redirect_to root_url, notice: "Thank you for signing up!"
+          redirect_back fallback_location: request.referrer, notice: "Thank you for signing up!"
         else
           render "new"
         end
