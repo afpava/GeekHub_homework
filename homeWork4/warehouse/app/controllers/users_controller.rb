fe6e5ed4bb0ad36f8b0c31ae408ca330f26bbb28
pVa <pva@netbook>
2018-11-17 21:08:48 +0200

    HW5 before bootstrap -integration

--- homeWork4/warehouse/app/controllers/users_controller.rb
+++ homeWork4/warehouse/app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
-wrap_parameters :user, include: [:name, :password, :password_confirmation]
+  wrap_parameters :user, include: [:name, :password, :password_confirmation]
+  before_action :user_params, only: [:edit]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
       def new
         @user = User.new
+      end
+
+      def index
+        @users = User.all
+      end
+
+      def edit
+
+      end
+
+      def destroy
+        @user.destroy
+        respond_to do |format|
+          format.html { redirect_to stores_url, notice: 'user was successfully destroyed.' }
+        #  format.json { head :no_content }
+        end
       end
 
       def create
@@ -17,6 +36,11 @@ wrap_parameters :user, include: [:name, :password, :password_confirmation]
       end
 
     private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
       def user_params
           params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
--- homeWork4/warehouse/app/models/user.rb
+++ homeWork4/warehouse/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 
-validates_uniqueness_of :email
+email_regex = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+validates :email, :presence => true, :uniqueness => true, :format => email_regex
+validates :password, format: { with: /(?=.*?[0-9])(?=.*?[A-Za-z]).+/,
+    message: "Password must contain at least one digit and one letter" }, length: { in: 3..6 }
 has_secure_password
 
 end
