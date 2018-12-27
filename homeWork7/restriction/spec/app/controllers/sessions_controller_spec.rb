47631cdfef5e4c37af58dcea294dc44512bf460f
pVa <pva@netbook>
2018-12-27 17:54:32 +0200

    hw 9 sessions_controller

--- homeWork7/restriction/spec/app/controllers/sessions_controller_spec.rb
+++ homeWork7/restriction/spec/app/controllers/sessions_controller_spec.rb
@@ -1,34 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+
+  # let(:user) { User.make(:username => 'bob') }
   let(:user) { User.create(email: 'test@test.com', nickname: 'Test', first_name: 'First', last_name: 'Super', birth_date: '01-01-1954', password:'123test',role:'admin' ) }
-  # let(:person1) {User.create(email:'tw@com.com',password:'123test',nickname: 'Twilight Sparkle', birth_date: '2006-09-09').reload}
-  # let(:person2) {User.create(email:'rd@com.com',password:'123test',nickname: 'Rainbow Dash',birth_date: '2006-09-08').reload}
 
-  let(:user_params) { user_params = { email: 'test@test.com',  password:'123test' } }
+  describe '#create' do
+    context 'authenticate fail' do
+      before(:each) { allow(controller).to receive(:require_login) {nil} }
 
-  before do
-    user_params
+      it 'session[:user_id] should == user.id' do
+        post :create
+      expect( session[:user_id] ).to be_nil
+      end
+
+      it 'should render_template :new' do
+        post :create
+        expect(response).to render_template :new
+      end
+
+      it 'flash.now[:alert] should be set to failure' do
+        post :create
+        expect(flash.now[:alert]).to eq 'Email or password is invalid'
+      end
+    end
+
+    context 'authenticate successfully' do
+      before(:each) { allow(controller).to receive(:require_login) {user} }
+
+      it 'should redirect' do
+        expects(:redirect_to_target_or_default).with(twikets_path)
+        post :create
+      end
+
+      it 'session[:user_id] should == user.id' do
+        post :create
+        session[:user_id].should == user.id
+      end
+
+      it 'flash[:notice] should be set to success' do
+        post :create
+        flash[:notice].should == "Logged in successfully."
+      end
+    end
   end
 
-  describe "#create" do
-
-    it "should successfully create a user" do
-      expect { post :create, params: { user: user_params }}.to redirect_to root_path
-    end
-
-    it "should successfully create a session" do
-      expect( {session[:user_id]}).to be_nil
-      post :create, params: { user: user_params }
-      expect {session[:user_id]}.not_to eq nil
-    end
-
-    # it "should redirect the user to the root url" do
-    #   post :create, provider: :github
-    #   response.should redirect_to root_url
-    # end
-
-  end # sessios#create
+  # # let(:person1) {User.create(email:'tw@com.com',password:'123test',nickname: 'Twilight Sparkle', birth_date: '2006-09-09').reload}
+  # # let(:person2) {User.create(email:'rd@com.com',password:'123test',nickname: 'Rainbow Dash',birth_date: '2006-09-08').reload}
+  #
+  # let(:user_params) { user_params = { email: 'test@test.com',  password:'123test' } }
+  #
+  # before do
+  #   user_params
+  # end
+  #
+  # describe "#create" do
+  #
+  #   it "should successfully create a user" do
+  #     expect { post :create, params: { user: user_params }}.to redirect_to root_path
+  #   end
+  #
+  #   it "should successfully create a session" do
+  #     expect( {session[:user_id]}).to be_nil
+  #     post :create, params: { user: user_params }
+  #     expect {session[:user_id]}.not_to eq nil
+  #   end
+  #
+  #   # it "should redirect the user to the root url" do
+  #   #   post :create, provider: :github
+  #   #   response.should redirect_to root_url
+  #   # end
+  #
+  # end # sessios#create
 
   # describe "#destroy" do
   #   before do
